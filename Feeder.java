e6aefe4eed7ec60bb43a9057e0b5d87e6c927fc0
leannec8 <149003503+leannec8@users.noreply.github.com>
2024-09-19 13:32:39 +0000

    hi

--- Feeder.java
+++ Feeder.java
@@ -19,6 +19,13 @@ public class Feeder
 
     public void simulateOneDay(int numBirds)
     {
+        if(Math.random() < 0.05) currentFood = 0;
+        else
+        {
+            int eaten = (int) (Math.random() * 41) + 10;
+            currentFood -= eaten * numBirds;
+            if(currentFood < 0) currentFood = 0;
+        }
     }
 
     /**
@@ -29,6 +36,7 @@ public class Feeder
 
     public int simulateManyDays(int numBirds, int numDays) 
     {
+        int days = 0;
         return 0;
     }
 
@@ -38,4 +46,9 @@ public class Feeder
     }
 
     public Feeder() {}
+    
+    public String toString()
+    {
+        return "Current Food: " + currentFood;
+    }
 }
--- Main.java
+++ Main.java
@@ -2,16 +2,8 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Feeder f = new Feeder();
-        System.out.println(f);
-        System.out.println(f.getCurrentFood());
-        Feeder g = new Feeder(500);
-        System.out.println(g.getCurrentFood());
-        g.simulateOneDay(12);
-        System.out.println(Math.random()); //prints random decimal value from [0 to 1)
-        double random = Math.random() * 10;
-        System.out.println(random); // prints random decimal between 1 and 10
-        int random = (int) (Math.random() * 10);
-        System.out.println(random);
+        Feeder i = new Feeder(100);
+        i.simulateOneDay(5);
+        System.out.println(i);
     }
 }
